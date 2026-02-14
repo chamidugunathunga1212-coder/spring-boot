2d7aa047c387fd9814867b1d8c9897874edcc96a
Chamidu Gunathunga <chamidugunathunga1212@gmail.com>
2026-02-14 18:45:20 +0530

    add first task

---
+++ demo/src/main/java/com/paf/demo/DemoApplication.java
@@ -0,0 +1,23 @@
+package com.paf.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication // This annotation indicates that this is a Spring Boot application. It enables auto-configuration and component scanning.
+
+@RestController // This annotation indicates that this class will handle HTTP requests and return responses directly from the methods, without needing to use a separate view or template.
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@GetMapping("/hello")
+	public String rootEndpoint() {
+		return "Hello, World!";
+	}
+
+}
+
